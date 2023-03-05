b040c5d5740f54f31b20ad831d6953ce5de0cb72
DSulzhits <dsulzhits@gmail.com>
2023-03-05 16:17:30 +0300

    Добавлены строки кода

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from classes.classes import Node, Stack
+from classes.stack import Node, Stack
+from classes.custom_queue import Queue
 
 
 def main():
@@ -16,19 +17,32 @@ def main():
     # print(stack.top.next_node.data)
     # print(stack.top.next_node.next_node.data)
     # print(stack.top.next_node.next_node.next_node)
-    stack = Stack()
-    stack.push('data1')
-    data = stack.pop()
-    print(stack.top)
-    print(data)
-    stack = Stack()
-    stack.push('data1')
-    stack.push('data2')
-    data = stack.pop()
-    print(stack.top.data)
-    print(data)
-    data = stack.pop()
-    print(data)
+    # stack = Stack()
+    # stack.push('data1')
+    # data = stack.pop()
+    # print(stack.top)
+    # print(data)
+    # stack = Stack()
+    # stack.push('data1')
+    # stack.push('data2')
+    # data = stack.pop()
+    # print(stack.top.data)
+    # print(data)
+    # data = stack.pop()
+    # print(data)
+    queue = Queue()
+    queue.enqueue('data1')
+    queue.enqueue('data2')
+    queue.enqueue('data3')
+
+    print(queue.head.data)
+    print(queue.head.next_node.data)
+    print(queue.tail.data)
+    print(queue.tail.next_node)
+    print(queue.tail.next_node.data)
+
+
+
 
 
 if __name__ == "__main__":
